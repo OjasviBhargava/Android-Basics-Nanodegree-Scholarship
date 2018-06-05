e69783ceebd131269bca02c2505f0688b4224aab
Ojasvi Bhargava <ojasvi.bhargava96@gmail.com>
2018-06-05 18:18:47 +0530

    Initial Commit

---
+++ HarisonsInt/app/src/main/java/com/example/ojasvi/harisonsint/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.ojasvi.harisonsint;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    String phoneNumber ="9814129544";
+
+    public void dialPhoneNumber(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void website(View view) {
+        String url = "http://www.harisoninternational.com";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+
+}
+
+
+

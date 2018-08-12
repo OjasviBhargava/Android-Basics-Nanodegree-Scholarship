bd469651dd416c4e42cddb0cd5a68e00bb650d03
Ojasvi Bhargava <ojasvi.bhargava96@gmail.com>
2018-08-12 19:43:12 +0530

    added remaining miwok app files

--- Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,18 +1,25 @@
 package com.example.android.miwok;
-
+import android.widget.ImageView;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.support.v4.content.ContextCompat;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
 public class WordAdapter extends ArrayAdapter {
 
-    public WordAdapter(Context context, ArrayList pWords) {
+    /** Resource ID for the background color for this list of words */
+    private int mColorResourceId;
+
+
+    public WordAdapter(Context context, ArrayList pWords, int colorResourceId) {
         super(context,0, pWords);
+        mColorResourceId = colorResourceId;
     }
 
     @Override
@@ -30,7 +37,24 @@ public class WordAdapter extends ArrayAdapter {
         miwokTextView.setText(currentWord.getMiwokTranslation());
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWord.getDefaultTranslation());
-
+        // Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        // Check if an image is provided for this word or not
+        if (currentWord.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
         return listItemView;
     }
 }
